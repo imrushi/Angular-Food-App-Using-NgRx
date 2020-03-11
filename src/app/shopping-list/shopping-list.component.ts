f44619b7686e64aea59f129ecbb32bd2aaddb73f
Rushi Panchariya <rushirock2000@outlook.com>
2020-03-11 11:42:00 +0530

    Services for Shopping list Pushing data in ingredient array

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from './shopping-list.service';
 
 @Component({
 	selector: 'app-shopping-list',
@@ -7,12 +8,13 @@ import { Ingredient } from '../shared/ingredient.model';
 	styleUrls: [ './shopping-list.component.css' ]
 })
 export class ShoppingListComponent implements OnInit {
-	ingredients: Ingredient[] = [ new Ingredient('Apples', 5), new Ingredient('Tomatoes', 10) ];
-	constructor() {}
+	ingredients: Ingredient[];
+	constructor(private slService: ShoppingListService) {}
 
-	ngOnInit() {}
-
-	onIngredientAdded(ingredient: Ingredient) {
-		this.ingredients.push(ingredient);
+	ngOnInit() {
+		this.ingredients = this.slService.getIngredients();
+		this.slService.ingredientsChanged.subscribe((ingredients: Ingredient[]) => {
+			this.ingredients = ingredients;
+		});
 	}
 }
