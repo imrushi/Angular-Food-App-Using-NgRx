fb81988c5882952c7d1b196ff01dff150ae3b848
Rushi Panchariya <rushirock2000@outlook.com>
2020-03-24 22:29:01 +0530

    Service is done

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,22 +1,33 @@
 import { Recipe } from './recipe.model';
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
 	recipeSelected = new EventEmitter<Recipe>();
 	private recipes: Recipe[] = [
 		new Recipe(
-			'A Test Recipe',
-			'This is simply a test',
-			'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'
+			'Tasty Schnitzel',
+			'A super-tasty Schnitzel - just awesome!',
+			'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
+			[ new Ingredient('Meat', 1), new Ingredient('French Fries', 20) ]
 		),
 		new Recipe(
-			'Another Test Recipe',
-			'This is simply a test',
-			'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'
+			'Big Fat Burger',
+			'What else you need to say?',
+			'https://www.fatburgercanada.com/wp-content/uploads/2018/09/fb18_FatCheeseBeer.png',
+			[ new Ingredient('Buns', 2), new Ingredient('Meat', 1) ]
 		)
 	];
 
+	constructor(private slService: ShoppingListService) {}
+
 	getRecipes() {
 		return this.recipes.slice();
+	}
+
+	addIngredientsToShoppingList(ingredients: Ingredient[]) {
+		this.slService.addIngredients(ingredients);
 	}
 }
